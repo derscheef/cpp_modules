237496327395062541eb618b6a0be4bc809a6e50
derscheef <yannic.scheef@gmail.com>
2024-01-23 17:04:02 +0100

    Initial commit

---
+++ template/headerfiles/Class.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Class.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yscheef <yscheef@student.42vienna.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/20 23:56:58 by yscheef           #+#    #+#             */
+/*   Updated: 2024/01/22 04:06:22 by yscheef          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include "Head.hpp"
+
+class Item
+{
+private:
+public:
+    Item();
+    virtual ~Item();
+};
+
+#endif
